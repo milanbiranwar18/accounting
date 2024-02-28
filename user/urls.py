fd3afaed0f425f251178bbf38590785b965be775
Milan Biranwar <milanbiranwar06@gmail.com>
2024-02-28 20:46:29 +0530

    [User] Add user registration, login and logout

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    # path('user_registration/', views.Registration.as_view(), name='registration'),
-    # path('user_login/', views.Login.as_view(), name='login'),
-    # path('user_logout/', views.Logout.as_view(), name='user_logout'),
+    path('user_registration/', views.Registration.as_view(), name='registration'),
+    path('user_login/', views.Login.as_view(), name='login'),
+    path('user_logout/', views.Logout.as_view(), name='user_logout'),
 
 
 ]
--- user/views.py
+++ user/views.py
@@ -1,69 +1,69 @@
-# # Create your views here.
-# import json
-# import logging
-#
-# from user.utils import SessionAuth, SessionAuthentication, JWT
-# from django.contrib.auth import logout, login
-# from django.db import connection
-# from django.http import JsonResponse
-# from django.views.decorators.csrf import csrf_exempt
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-#
-#
-# from user.serializer import RegistrationSerializer, LoginSerializer
-#
-# logging.basicConfig(filename="django.log",
-#                     filemode='a',
-#                     format='%(asctime)s %(levelname)s-%(message)s',
-#                     datefmt='%Y-%m-%d %H:%M:%S')
-#
-#
-# class Registration(APIView):
-#     """
-#     Class for user Registration
-#     """
-#
-#     def post(self, request):
-#         try:
-#             serializer = RegistrationSerializer(data=request.data)
-#             serializer.is_valid(raise_exception=True)
-#             serializer.save()
-#             return Response({"message": "Registered Successfully", "data": serializer.data, "status": 201})
-#         except Exception as e:
-#             logging.error(e)
-#             return Response({"message": str(e)}, status=400)
-#
-#
-# class Login(APIView):
-#     """
-#     Class for user login
-#     """
-#
-#     def post(self, request):
-#         try:
-#
-#             serializer = LoginSerializer(data=request.data)
-#             serializer.is_valid(raise_exception=True)
-#             serializer.save()
-#             login(request, serializer.context.get("user"))
-#             user = serializer.context.get("user")
-#             token = JWT().encode(data={"user_id": user.id})
-#             return Response({"message": "Login Successfully", "token":token, "status": 201})
-#
-#         except Exception as e:
-#             logging.error(e)
-#             return Response({"message": str(e)}, status=400)
-#
-#
-# class Logout(APIView):
-#     """
-#     Class for user logout
-#     """
-#
-#     def get(self, request):
-#         if request.user.is_authenticated:
-#             logout(request)
-#             return Response({"Message": "Logout Successfully", "status": 200})
-#         return Response({"Message": "User already logout"})
-#
+# Create your views here.
+import json
+import logging
+
+from user.utils import SessionAuth, SessionAuthentication, JWT
+from django.contrib.auth import logout, login
+from django.db import connection
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+from user.serializer import RegistrationSerializer, LoginSerializer
+
+logging.basicConfig(filename="django.log",
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s-%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+
+class Registration(APIView):
+    """
+    Class for user Registration
+    """
+
+    def post(self, request):
+        try:
+            serializer = RegistrationSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({"message": "Registered Successfully", "data": serializer.data, "status": 201})
+        except Exception as e:
+            logging.error(e)
+            return Response({"message": str(e)}, status=400)
+
+
+class Login(APIView):
+    """
+    Class for user login
+    """
+
+    def post(self, request):
+        try:
+
+            serializer = LoginSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            login(request, serializer.context.get("user"))
+            user = serializer.context.get("user")
+            token = JWT().encode(data={"user_id": user.id})
+            return Response({"message": "Login Successfully", "token":token, "status": 201})
+
+        except Exception as e:
+            logging.error(e)
+            return Response({"message": str(e)}, status=400)
+
+
+class Logout(APIView):
+    """
+    Class for user logout
+    """
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            logout(request)
+            return Response({"Message": "Logout Successfully", "status": 200})
+        return Response({"Message": "User already logout"})
+
