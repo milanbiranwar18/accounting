a37b30cecba43d21f31d164d554229fb047e2c42
Milan Biranwar <milanbiranwar06@gmail.com>
2024-02-28 20:45:13 +0530

    [User] Add user registration, login and logout

---
+++ user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # path('user_registration/', views.Registration.as_view(), name='registration'),
+    # path('user_login/', views.Login.as_view(), name='login'),
+    # path('user_logout/', views.Logout.as_view(), name='user_logout'),
+
+
+]
